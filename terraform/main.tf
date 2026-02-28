6579736cb1a4c374a17d7ee4126b48058d970a06
Yijin Wang <yijin.wang2003@gmail.com>
2026-02-27 22:25:31 -0600

    Fix: restrict public ingress and egress

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,19 +5,20 @@ provider "aws" {
 resource "aws_security_group" "demo_sg" {
   name        = "demo_sg"
   description = "Security group for infra validation demo"
+
   ingress {
-    description = "Allow HTTP only"
+    description = "Allow HTTP from internal network only"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Restrict outbound traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
