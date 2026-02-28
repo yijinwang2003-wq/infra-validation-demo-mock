82699abf20f209158309ec22d7d3565533dfdbc1
Yijin Wang <yijin.wang2003@gmail.com>
2026-02-27 22:22:54 -0600

    Fix: format terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "demo_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
