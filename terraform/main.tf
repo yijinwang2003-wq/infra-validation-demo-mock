02842da21dec5031e54abe125fa4b7acd567e8ae
Yijin Wang <yijin.wang2003@gmail.com>
2026-02-27 22:20:21 -0600

    Fix: remove open SSH access

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,12 @@ provider "aws" {
 resource "aws_security_group" "demo_sg" {
   name        = "demo_sg"
   description = "Security group for infra validation demo"
-
   ingress {
-    description = "SSH from anywhere (intentionally insecure for demo)"
-    from_port   = 22
-    to_port     = 22
+    description = "Allow HTTP only"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # <-- 漏洞点：tfsec 必抓
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
